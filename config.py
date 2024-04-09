39cddc6b3c6981ebfa7a2f2942b636d2fbac1834
Sayanov Ramazan <sayanov.ramazan@gmail.com>
2024-04-10 01:40:28 +0300

    machine translation

---
+++ config.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+
+
+def get_config():
+    return {
+        "batch_size": 8,
+        "num_epochs": 15,
+        "lr": 1e-4,
+        "seq_len": 250,
+        "d_model": 512,
+        "lang_src": "en",
+        "lang_tgt": "ru",
+        "model_folder": "weights",
+        "model_basename": "tmodel_",
+        "preload": None,
+        "tokenizer_file": "tokenizer_{0}.json",
+        "experiment_name": "runs/tmodel"
+    }
+
+
+def get_weights_files_path(config, epoch: str):
+    model_folder = config["model_folder"]
+    model_basename = config["model_basename"]
+    model_filename = f"{model_basename}{epoch}.pt"
+    return str(Path('.') / model_folder / model_filename)
+
+
+def latest_weights_file_path(config):
+    model_folder = f"{config['datasource']}_{config['model_folder']}"
+    model_filename = f"{config['model_basename']}*"
+    weights_files = list(Path(model_folder).glob(model_filename))
+    if len(weights_files) == 0:
+        return None
+    weights_files.sort()
+    return str(weights_files[-1])
